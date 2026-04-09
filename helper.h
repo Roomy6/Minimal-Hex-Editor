9f2c68d5188a61b07ea905ffc5c79c76d82cef14
Roomy6 <roomyk792@gmail.com>
2026-04-09 20:09:22 +0200

    Added some helper functions

--- helper.h
+++ helper.h
@@ -2,8 +2,28 @@
 #define HELPER_H
 
 #include <stdio.h>
+#include <stdbool.h>
 
 #define DISPLAY_SIZE 16
+
+extern bool debug;
+extern bool showAscii;
+extern bool showAddress;
+
+void dbgPrint(char string[])
+{
+    if(debug)
+        printf("[DBG] %s\n", string);
+}
+
+void addressPrint(long address, bool nl)
+{
+    if(showAddress)
+        if(nl)
+            printf("%08lx: \n", address);
+        else if(!nl)
+            printf("%08lx: ", address);
+}
 
 int hexToAscii(size_t bytesRead, unsigned char buffer[DISPLAY_SIZE])
 {
